0282dcf3f7d0f5b9d8ec7a88cb9abbab5d4d767f
Juan José Rendón <juan.jose@netmidas.com>
2018-11-05 15:24:27 -0500

    hook applied

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import Form from "./components/Form";
 
-import './App.css';
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-      </div>
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const toggleComplete = i => {
+    setTodos(
+      todos.map(
+        (todo, k) =>
+          k === i
+            ? {
+                ...todo,
+                complete: !todo.complete
+              }
+            : todo
+      )
     );
-  }
-}
+  };
+  return (
+    <div className="App">
+      <Form
+        onSubmit={text => setTodos([{ text, complete: false }, ...todos])}
+      />
+      <div>
+        {todos.map(({ text, complete }, i) => (
+          <div
+            key={Math.random() * 10}
+            onClick={() => toggleComplete(i)}
+            style={{
+              textDecoration: complete ? "line-through" : ""
+            }}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => setTodos([])}>Reset</button>
+    </div>
+  );
+};
 
 export default App;
