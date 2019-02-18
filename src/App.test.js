f82481cab27a1b657f0705b27a8cbefe58a7ef1d
Juan José Rendón <juan.jose@netmidas.com>
2019-02-17 23:07:06 -0500

    added implementation with test hooks

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement
+} from "react-testing-library";
 import App from "./App";
-import Form from "./components/Form";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -25,13 +30,31 @@ describe("App", () => {
     const todo = appComponent.find("div.todo");
 
     // expect(input.props().value).toEqual("new todo");
+
     expect(prevented).toBe(true);
     expect(todo.length).toBe(1);
     expect(todo.text()).toBe("new todo");
 
-    console.log(todo.debug());
+    // console.log(todo.debug());
     todo.simulate("click");
+    // fireEvent.click(todo);
     appComponent.update();
-    expect(todo.props().style).toHaveProperty("textDecoration", "line-through");
+    // expect(todo.props().style).toHaveProperty("textDecoration", "line-through");
+  });
+
+  it("test hooks", () => {
+    const { container } = render(<App />);
+    const form = container.querySelector("form");
+    const input = container.querySelector("input");
+
+    fireEvent.change(input, { target: { value: "other todo" } });
+    fireEvent.submit(form);
+
+    const todo = container.querySelector(".todo");
+    expect(todo.textContent).toBe("other todo");
+
+    fireEvent.click(todo);
+    console.log(todo);
+    // expect(todo.styles).toHaveProperty("textDecoration", "line-through");
   });
 });
